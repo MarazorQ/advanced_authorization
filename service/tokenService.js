048b4ad23604b2c06bab2c53e5a337ea27a98c69
artem shkuratov <shuratov2001@mail.ru>
2021-07-09 12:50:19 +0300

    init registration and added user Data transfer object

--- service/tokenService.js
+++ service/tokenService.js
@@ -1,5 +1,24 @@
-class TokenService{
+const jwt = require('jsonwebtoken')
+const tokenModel = require('../models/tokenModel')
 
+class TokenService{
+    generateTokens(payload){
+        const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY_ACCESS, {expiresIn: '24h'})
+        const refreshToken = jwt.sign(payload, process.env.JWT_SECRET_KEY_REFRESH, {expiresIn: '24d'})
+        return {
+            accessToken,
+            refreshToken
+        }
+    }
+    async saveToken(userID, refresh){
+        const tokenData = await tokenModel.findOne({user: userID})
+        if (tokenData){
+            tokenData.refreshToken = refresh
+            return tokenData.save()
+        }
+        const token = await tokenModel.create({user: userID, refresh})
+        return token
+    }
 }
 
 module.exports = new TokenService()
--- service/userService.js
+++ service/userService.js
@@ -1,8 +1,22 @@
 const UserModel = require('../models/userModel.js')
 
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+
+const mailService = require('./mailService.js')
+const tokenService = require('./tokenService.js')
+
 class UserService{
     async registration(email, password){
-        
+        const user = await UserModel.findOne({email})
+        if (user){
+            throw new Error('Пользователь с таким именем уже зарегестрирован')
+        }
+        const hash_password = await bcrypt.hash(password, 3)
+        const activation_Link = uuid.v4()// random string // v34fa-asf32-dasf=dasdas 
+        const new_user = await UserModel.create({email, password: hash_password, activation_Link})
+        await mailService.sendActivationMail(email, activation_Link)
+        const token_pair = tokenService.generateTokens
     }
 }
 
