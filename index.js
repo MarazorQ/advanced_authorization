86485d516b037227f47fac52723f1d66e3fedbe1
artem shkuratov <shuratov2001@mail.ru>
2021-07-09 11:41:41 +0300

    added db

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-const port = process.env.PORT
+const port = process.env.PORT || 7000
+const db = process.env.DB_URL
+
 const app = express()
 
 const start = async () =>{
     try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         app.listen(port, () => console.log(`Server started on ${port} port...`))
     }catch(e){
         console.log('EROR', e)
